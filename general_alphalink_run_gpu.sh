6acd4cfcbaf197fbf0f5ef5323894f8fe0cd49fa
Camilla Paleari <cpaleari@eu-login-21.euler.ethz.ch>
2024-07-16 15:46:20 +0200

    Final

--- general_alphalink_run_gpu.sh
+++ general_alphalink_run_gpu.sh
@@ -4,7 +4,7 @@
 #SBATCH -n 1
 #SBATCH --cpus-per-task=16
 #SBATCH --gpus=1
-#SBATCH --time=4:00:00              # Time limit hrs:min:sec
+#SBATCH --time=00:30:00              # Time limit hrs:min:sec
 #SBATCH --mem-per-cpu=2000
 #SBATCH --tmp=4000
 sleep 2
@@ -16,13 +16,13 @@ module load gcc
 # Define variables for easy changes
 GENE_NAME=$1
 PROTEIN_ID=$2
-FOLDER_NAME="${GENE_NAME}_${PROTEIN_ID}_true"  # Construct folder name dynamically
+FOLDER_NAME="Alphalink1_only_true2/${GENE_NAME}_${PROTEIN_ID}"  # Construct folder name dynamically
 SUBFOLDER_NAME=$3
 
 PYTHON_ENV_PATH="/cluster/home/cpaleari/miniconda3/envs/alphalink_env2"
 RESOURCES_PATH="/cluster/scratch/cpaleari/resources"
 ALPHAFOLD_DB_PATH="/cluster/project/alphafold"
-OUTPUT_POSTFIX="${SUBFOLDER_NAME}_1neff"
+OUTPUT_POSTFIX="${SUBFOLDER_NAME}_neff5"
 
 export PYTHON_PATH=$PYTHON_PATH:${PYTHON_ENV_PATH}/bin/python3.7
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${PYTHON_ENV_PATH}/lib
@@ -37,4 +37,4 @@ cd /cluster/scratch/cpaleari/AlphaLink/
 python preprocessing_distributions.py --infile /cluster/scratch/cpaleari/AlphaLink/${FOLDER_NAME}/${SUBFOLDER_NAME}/restraints.csv --outfile /cluster/scratch/cpaleari/AlphaLink/${FOLDER_NAME}/${SUBFOLDER_NAME}/restraint_distributions.csv
 
 
-python predict_with_crosslinks.py /cluster/scratch/cpaleari/AlphaLink/${FOLDER_NAME}/${PROTEIN_ID}.fasta /cluster/scratch/cpaleari/AlphaLink/${FOLDER_NAME}/${SUBFOLDER_NAME}/restraint_distributions.csv --distograms --data_random_seed 0 --neff 1 --checkpoint_path ${RESOURCES_PATH}/Alphalink_params/finetuning_model_5_ptm_distogram.pt --output_dir /cluster/scratch/cpaleari/AlphaLink/${FOLDER_NAME}/${SUBFOLDER_NAME}/ --output_postfix ${OUTPUT_POSTFIX} --uniref90_database_path ${ALPHAFOLD_DB_PATH}/uniref90/uniref90.fasta --mgnify_database_path ${ALPHAFOLD_DB_PATH}/mgnify/mgy_clusters_2022_05.fa  --pdb70_database_path ${ALPHAFOLD_DB_PATH}/pdb70/pdb70 --uniclust30_database_path ${ALPHAFOLD_DB_PATH}/uniclust30/uniclust30_2018_08 --jackhmmer_binary_path ${PYTHON_ENV_PATH}/bin/jackhmmer  --hhblits_binary_path ${PYTHON_ENV_PATH}/bin/hhblits  --hhsearch_binary_path ${PYTHON_ENV_PATH}/bin/hhsearch  --kalign_binary_path ${PYTHON_ENV_PATH}/bin/kalign
+python predict_with_crosslinks.py /cluster/scratch/cpaleari/AlphaLink/${FOLDER_NAME}/${PROTEIN_ID}.fasta /cluster/scratch/cpaleari/AlphaLink/${FOLDER_NAME}/${SUBFOLDER_NAME}/restraint_distributions.csv --distograms --data_random_seed 0 --neff 5 --checkpoint_path ${RESOURCES_PATH}/Alphalink_params/finetuning_model_5_ptm_distogram.pt --output_dir /cluster/scratch/cpaleari/AlphaLink/${FOLDER_NAME}/${SUBFOLDER_NAME}/ --output_postfix ${OUTPUT_POSTFIX} --uniref90_database_path ${ALPHAFOLD_DB_PATH}/uniref90/uniref90.fasta --mgnify_database_path ${ALPHAFOLD_DB_PATH}/mgnify/mgy_clusters_2022_05.fa  --pdb70_database_path ${ALPHAFOLD_DB_PATH}/pdb70/pdb70 --uniclust30_database_path ${ALPHAFOLD_DB_PATH}/uniclust30/uniclust30_2018_08 --jackhmmer_binary_path ${PYTHON_ENV_PATH}/bin/jackhmmer  --hhblits_binary_path ${PYTHON_ENV_PATH}/bin/hhblits  --hhsearch_binary_path ${PYTHON_ENV_PATH}/bin/hhsearch  --kalign_binary_path ${PYTHON_ENV_PATH}/bin/kalign
